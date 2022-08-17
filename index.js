2f4b1a5e92e83b71c0177347d89c405a4e637b51
Aditya Veer <nakulparmar15@gmail.com>
2022-08-17 23:01:43 +0530

    template

--- index.js
+++ index.js
@@ -14,45 +14,10 @@ app.use(bodyParser.urlencoded({ limit: "8mb", extended: false }));
 app.use(cors({ credentials: true, origin: "https://localhost:3000" }));
 app.use(cookieParser());
 
-const partical_client = "https://shared.partical.xyz/server1";
+const partical_client = "";
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
-});
-
-app.post("/insertEncrypted", async (req, res) => {
-  const client = new Client("https://shared.partical.xyz/server1");
-  // encrypt all fields
-  const encrypted = Object.keys(req.body.data).map((key) => {
-    return {
-      [key]: cryptr.encrypt(req.body.data[key]),
-    };
-  });
-  const payload = {
-    ...req.body,
-    data: Object.assign({}, ...encrypted),
-  };
-  const data = await client.createDB(req, "page", payload);
-  console.log(data.data);
-  res.send(data.data);
-});
-
-app.get("/getEncrypted/:pageId", async (req, res) => {
-  const client = new Client("https://shared.partical.xyz/server1");
-  const result = await client.getDB(req, "page", req.params.pageId);
-  const decrypted = Object.keys(result.data).map((key) => {
-    try {
-      return {
-        [key]: cryptr.decrypt(result.data[key]),
-      };
-    } catch (e) {
-      return {
-        [key]: result.data[key],
-      };
-    }
-  });
-  const data = Object.assign({}, ...decrypted);
-  res.send(data);
 });
 
 app.listen(port, () => {
