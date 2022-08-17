1c4fd243a500306e0428c044b88002a19fb47718
Aditya Veer <nakulparmar15@gmail.com>
2022-08-17 16:41:00 +0530

    real time?

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/create", async (req, res) => {
 
 app.get("/", async (req, res) => {
   // console.log("a");
-  res.send("Hello World5.1!");
+  res.send("Hello World7!");
 });
 
 app.listen(port, () => {
