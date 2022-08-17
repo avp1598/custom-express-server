9f00bb5039a30dc75fca0e310791820aa3acd17c
Aditya Veer <nakulparmar15@gmail.com>
2022-08-17 12:31:20 +0530

    test

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/create", async (req, res) => {
 
 app.get("/", async (req, res) => {
   // console.log("a");
-  res.send("Hello World1!");
+  res.send("Hello World2!");
 });
 
 app.listen(port, () => {
